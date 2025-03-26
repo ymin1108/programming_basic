9671ab37b83e48f1d98083edaa5a80ef50e59468
IMYMIN <asw6466@gmail.com>
2025-03-26 11:31:51 +0900

    Available print return type

--- ast.c
+++ ast.c
@@ -29,34 +29,52 @@ int main(void)
 
     // json 객체 생성
     json_value json = json_create(buffer);
-    // json_print(json);
 
     json_value ext = json_get(json, "ext");
     int ext_size = json_len(ext);
 
-
     // ext의 value 값들
     for (int i = 0; i < ext_size; i++)
     {
-        // extdml value 값
         json_value ext_index = json_get(ext, i);
-
-        /*
-        _nodetype이 FuncDef 또는 Decldls 일 때만 실행
-        _nodetype = FuncDef
-        _nodetype = Decl > type._nodetype = FuncDecl
-         위 두 경우가 함수
-        */
         char *nodetype_str = json_get_string(ext_index, "_nodetype");
+        
         if (!strcmp(nodetype_str, "FuncDef"))
         {
-            number_of_funcdef++;
-            
-            json_value funcdef_decl = json_get(ext_index,"decl");
-
-
+            json_value funcdef_decl = json_get(ext_index, "decl");
             char *funcdef_decl_str = json_get_string(funcdef_decl, "name");
-            printf("%s\n", funcdef_decl_str);
+            
+            json_value funcdef_type = json_get(funcdef_decl, "type");
+            
+            // 반환 타입이 포인터인지 확인
+            char *type_nodetype = json_get_string(funcdef_type, "type", "_nodetype");
+            
+            if (!strcmp(type_nodetype, "PtrDecl")) {
+                // 포인터 타입인 경우
+                json_value ptr_type = json_get(funcdef_type, "type");
+                json_value type_decl = json_get(ptr_type, "type");
+                json_value type_value = json_get(type_decl, "type");
+                json_value names_array = json_get(type_value, "names");
+                
+                json_value first_name = json_get(names_array, 0);
+                
+                printf("%s\n\tReturn Type: ", funcdef_decl_str);
+                json_print(first_name);
+                printf("*\n");
+            } else {
+                // 일반 타입인 경우
+                json_value funcdef_type_type = json_get(funcdef_type, "type");
+                json_value names_array = json_get(funcdef_type_type, "type");
+                names_array = json_get(names_array, "names");
+                
+                json_value first_name = json_get(names_array, 0);
+                
+                printf("%s\n\tReturn Type: ", funcdef_decl_str);
+                json_print(first_name);
+                printf("\n");
+            }
+            
+            number_of_funcdef++;
         }
         
         if (!strcmp(nodetype_str, "Decl"))
@@ -65,17 +83,43 @@ int main(void)
             char *decl_type_nodetype = json_get_string(decl_type, "_nodetype");
 
             if (!strcmp(decl_type_nodetype, "FuncDecl"))
-            {   
-                number_of_funcdef++;
+            {
                 char *decl_name = json_get_string(ext_index, "name");
-                printf("%s\n", decl_name);
+                
+                // 반환 타입이 포인터인지 확인
+                char *type_nodetype = json_get_string(decl_type, "type", "_nodetype");
+                
+                if (!strcmp(type_nodetype, "PtrDecl")) {
+                    // 포인터 타입인 경우
+                    json_value ptr_type = json_get(decl_type, "type");
+                    json_value type_decl = json_get(ptr_type, "type");
+                    json_value type_value = json_get(type_decl, "type");
+                    json_value names_array = json_get(type_value, "names");
+                    
+                    json_value first_name = json_get(names_array, 0);
+                    
+                    printf("%s\n\tReturn Type: ", decl_name);
+                    json_print(first_name);
+                    printf("*\n");
+                } else {
+                    // 일반 타입인 경우
+                    json_value decl_type_type = json_get(decl_type, "type");
+                    json_value names_array = json_get(decl_type_type, "type");
+                    names_array = json_get(names_array, "names");
+                    
+                    json_value first_name = json_get(names_array, 0);
+                    
+                    printf("%s\n\tReturn Type: ", decl_name);
+                    json_print(first_name);
+                    printf("\n");
+                }
+                
+                number_of_funcdef++;
             }
         }
     }
     
-    
     printf("\nNumber of function: %d", number_of_funcdef);
-
 
     // 메모리 해제
     free(buffer);
